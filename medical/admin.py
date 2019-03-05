13d994fa97eeef2f8cd3e3cfe2475ac2f19b07be
siuols <louisaleksieb.dagusen@gmail.com>
2019-03-06 00:23:49 +0800

    Register models to admin

--- medical/admin.py
+++ medical/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Course, Department, Stock, Patient, History
 # Register your models here.
+
+admin.site.register(Course)
+admin.site.register(Department)
+admin.site.register(Stock)
+admin.site.register(Patient)
+admin.site.register(History)
